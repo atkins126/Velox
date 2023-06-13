a74f85cf5f6b529da20bb17203e6b020186fc601
Francisco Sáez <fjsaez@gmail.com>
2023-06-12 21:54:00 -0400

    Creada unidad UtilesVelox

--- Velox.dpr
+++ Velox.dpr
@@ -1,12 +1,14 @@
 ﻿program Velox;
 
 uses
-  System.StartUpCopy, System.SysUtils,
+  System.StartUpCopy,
+  System.SysUtils,
   Androidapi.JNI.App,
   Androidapi.JNI.GraphicsContentViewText,
   Androidapi.Helpers,
   FMX.Forms,
-  Principal in 'Principal.pas' {FPrinc};
+  Principal in 'Principal.pas' {FPrinc},
+  UtilesVelox in 'UtilesVelox.pas';
 
 {$R *.res}
 
